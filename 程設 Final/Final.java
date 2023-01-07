7fc6c77df9e8256a3980d8007bc40597f3acd7c7
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2023-01-07 17:28:45 +0800

    Update Final.java

--- 程設 Final/Final.java
+++ 程設 Final/Final.java
@@ -16,13 +16,13 @@ public class Final {
 
             while (!input.equalsIgnoreCase("q")) {
                 input = sc.nextLine();
-                String[] sep = input.split(" ");
+                String[] arr = input.split(" ");
 
                 try {
-                    if (input.split(" ")[0].equals("a") && sep.length == 2) {
-                        avl.add(Integer.parseInt(sep[1]));
-                    } else if (input.split(" ")[0].equals("d") && sep.length == 2) {
-                        avl.del(Integer.parseInt(sep[1]));
+                    if (arr[0].equals("a") && arr.length == 2) {
+                        avl.add(Integer.parseInt(arr[1]));
+                    } else if (arr[0].equals("d") && arr.length == 2) {
+                        avl.del(Integer.parseInt(arr[1]));
                     } else if (!input.equalsIgnoreCase("q")) {
                         System.out.println("Invalid input. Please Enter again!!\n");
                     }
